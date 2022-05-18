30440d087d76d8aa15470b8deaa1f4e520ff00f0
Dominik <24534yrut@gmail.com>
2022-05-18 16:33:48 +0100

    added app Products

--- ProjectShop/Products/models.py
+++ ProjectShop/Products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    Id = models.IntegerField()
+    Date_added = models.DateField()
+    Is_listed = models.BooleanField()
+    Describe = models.TextField(max_length = 1000)
+    Image = models.ImageField()
+    Price = models.DecimalField(max_digits = 12, decimal_places = 2)
+    Product_name = models.CharField(max_length = 100)
+    Id_category = models.IntegerField()
+    Id_client = models.IntegerField()
