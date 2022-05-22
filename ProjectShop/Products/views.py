522979cecc6866cf76492bde53fa5cf4c0dcee85
Dominik <24534yrut@gmail.com>
2022-05-22 11:07:40 +0100

    base html

--- ProjectShop/Products/views.py
+++ ProjectShop/Products/views.py
@@ -10,3 +10,8 @@ class ProductCreateView(CreateView):
     template_name = "product_form.html"
     form_class = ProductForm
     success_url = reverse_lazy("book_create")
+
+
+def get_hello(request):
+    data = {}
+    return render(request, 'base.html', data)
