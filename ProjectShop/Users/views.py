10d9190316654a9f1aec21cb0901c67408a9db92
Dominik <24534yrut@gmail.com>
2022-05-22 14:46:58 +0100

    Registration side

--- ProjectShop/Users/views.py
+++ ProjectShop/Users/views.py
@@ -1,10 +1,12 @@
-from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
-from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from Users.forms import SignUpForm #type: ignore
 
 class Login_user(LoginView):
     template_name = 'login.html'
+
+class SignUpView(CreateView):
+    template_name = 'sign_up.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')
