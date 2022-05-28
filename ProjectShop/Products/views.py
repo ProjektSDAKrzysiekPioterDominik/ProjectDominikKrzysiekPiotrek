158a3091b3db9f5663cff5fcb15b16f05d52f788
krzysztof <projektinternetowy33@gmail.com>
2022-05-28 14:58:33 +0200

    products list

--- ProjectShop/Products/views.py
+++ ProjectShop/Products/views.py
@@ -5,7 +5,7 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from Products.forms import ProductForm
 from django.http import HttpResponse
-from .models import Products
+from .models import Products,Categories,Producer
 
 
 class ProductCreateView(CreateView):
@@ -15,8 +15,29 @@ class ProductCreateView(CreateView):
 
 
 def index(request):
-    zapytanie = Products.objects.all()
-    return HttpResponse(zapytanie)
+    all_products = Products.objects.all()
+    first = Products.objects.get(pk=1)
+    catego = Products.objects.filter(Id_category=2)
+    producer = Products.objects.filter(Producer=8)
+    catego_name = Categories.objects.get(id=2)
+    categories = Categories.objects.all()
+    null = Products.objects.filter(Id_category__isnull=False)
+    have = Products.objects.filter(Describe__icontains='lekko')
+    #return HttpResponse(all_products)
+
+    #context = {'categories' : categories}
+    return render(request,)
+
+def categorie(request, id):
+    categorie_user = Categories.objects.get(pk=id)
+    return HttpResponse(categorie_user.Name)
+
+def product(request,id):
+    product_user = Products.objects.get(pk=id)
+    textwrap = "<h1>" + str(product_user) + "</h1>" + \
+               "<p>" + str(product_user.Describe) + "</p>" + \
+               "<p>" + str(product_user.Price) + "</p>"
+    return HttpResponse(product_user)
 
 def get_hello(request):
     data = {}
--- ProjectShop/ProjectShop/urls.py
+++ ProjectShop/ProjectShop/urls.py
@@ -3,13 +3,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from Products.views import get_hello
-from Products.views import index
+from Products.views import *
 # from ProjectShop.Products.views import ProductCreateView
 
 urlpatterns = [
     path('', get_hello, name = 'home'),
     path('admin/', admin.site.urls),
     path('products/', include('Products.urls')),
-    path('products-list/', index)
+    path('products-list/', index, name='index'),
+    path('categorie/<id>/',categorie, name='categorie'),
+    path('product/<id>/',product, name='product'),
+
     # path('product-create', ProductCreateView.as_view(), name="product_create"),
 ]
