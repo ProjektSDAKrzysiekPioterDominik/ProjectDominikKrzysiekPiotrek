9963a6d10c5761260f8b3739c0766e8479a48027
krzysztof <projektinternetowy33@gmail.com>
2022-05-28 12:24:55 +0200

    Producers - added

--- ProjectShop/Products/urls.py
+++ ProjectShop/Products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from Products.views import ProductCreateView
-from Products.views import index
+
 
 
 urlpatterns = [
     path('product-create/', ProductCreateView.as_view(), name="product_create"),
-    path('products', index),
+
 ]
--- ProjectShop/ProjectShop/urls.py
+++ ProjectShop/ProjectShop/urls.py
@@ -3,12 +3,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from Products.views import get_hello
-
+from Products.views import index
 # from ProjectShop.Products.views import ProductCreateView
 
 urlpatterns = [
     path('', get_hello, name = 'home'),
     path('admin/', admin.site.urls),
     path('products/', include('Products.urls')),
+    path('products-list/', index)
     # path('product-create', ProductCreateView.as_view(), name="product_create"),
 ]
